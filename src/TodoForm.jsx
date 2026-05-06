46ded9e3c974eb1e27a91a9a7932830657a48b0b
jonastam93 <jonathan.stambuk@gmail.com>
2026-05-05 21:58:48 -0400

    commit

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,28 +1,25 @@
-import { useRef } from "react";
 import { useState } from "react";
 
 function TodoForm({ onAddTodo }) {
-    const inputRef = useRef();
 
 function handleAddTodo(event) {
         event.preventDefault();
 
         onAddTodo(workingTodoTitle);
 
-        setworkingTodoTitle('');
+        setWorkingTodoTitle('');
     };
-        const [workingTodoTitle, setworkingTodoTitle] = useState('');
+        const [workingTodoTitle, setWorkingTodoTitle] = useState('');
     return (
         <form onSubmit={handleAddTodo}>
             <label htmlFor="todoTitle">Todo</label>
             <input
-                ref={inputRef}
                 type="text"
                 id="todoTitle"
                 name="todoTitle"
                 placeholder={'Todo text'}
                 value={workingTodoTitle}
-                onChange={(event) => setworkingTodoTitle(event.target.value)}
+                onChange={(event) => setWorkingTodoTitle(event.target.value)}
                 required
             />
             <button type="submit" disabled={!workingTodoTitle.trim()}>
