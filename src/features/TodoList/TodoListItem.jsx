ef3188b123ba8cf75bc262adfbfd2b2e5e735eee
jonastam93 <jonathan.stambuk@gmail.com>
2026-05-12 20:51:14 -0400

    commit

--- src/features/TodoList/TodoListItem.jsx
+++ src/features/TodoList/TodoListItem.jsx
@@ -3,11 +3,33 @@ import TextInputWithLabel from "../../shared/TextInputWithLabel";
 
 function TodoListItem({ todo, onCompleteTodo }) {
   const [isEditing, setIsEditing] = useState(false);
+  const [workingTitle, setWorkingTitle] = useState(todo.title);
+
+  function handleCancel() {
+    setWorkingTitle(todo.title);
+    setIsEditing(false);
+  }
+
+  function handleEdit(event) {
+    setWorkingTitle(event.target.value);
+  }
 
   return (
     <li>
       {isEditing ? (
-        <TextInputWithLabel value={todo.title} />
+        <>
+          <TextInputWithLabel 
+            value={workingTitle} 
+            onChange={handleEdit}
+          />
+
+          <button
+            type="button"
+            onClick={handleCancel}
+          >
+            Cancel
+          </button>
+        </>
       ) : (
         <form>
           <label>
