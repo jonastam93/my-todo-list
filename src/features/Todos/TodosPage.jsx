7b12d7c553141dd624f5fd88fd90813c6be77074
jonastam93 <jonathan.stambuk@gmail.com>
2026-05-18 20:45:58 -0400

    commit

---
+++ src/features/Todos/TodosPage.jsx
@@ -0,0 +1,55 @@
+import './App.css';
+import TodoForm from './features/Todos/TodoForm';
+import TodoList from './features/Todos/TodoList';
+import { useState } from "react";
+
+function App() {
+  const [todoList, setTodoList] = useState([]);
+
+  function addTodo(todoTitle) {
+    const newTodo = {
+      id: Date.now(),
+      title: todoTitle,
+      isCompleted: false,
+    };
+
+    setTodoList((previous) => [newTodo, ...previous]);
+  }
+
+  function completeTodo(id) {
+    setTodoList((prevTodos) =>
+      prevTodos.map((todo) =>
+        todo.id === id
+          ? { ...todo, isCompleted: true }
+          : todo
+      )
+    );
+  }
+
+  function updateTodo(editedTodo) {
+    const updatedTodos = todoList.map((todo) => {
+      if (todo.id === editedTodo.id) {
+        return { ...editedTodo };
+      }
+      return todo;
+    });
+
+    setTodoList(updatedTodos);
+  }
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+
+      <TodoForm onAddTodo={addTodo} />
+
+      <TodoList
+        todoList={todoList}
+        onCompleteTodo={completeTodo}
+        onUpdateTodo={updateTodo}
+      />
+    </div>
+  );
+}
+
+export default App;
